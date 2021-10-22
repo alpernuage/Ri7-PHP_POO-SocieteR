178d0a87b6f673eb2ba1043885be0751a7d46fc9
alpernuage <alperakbulut@gmail.com>
2021-10-22 14:30:21 +0200

    modify Routes

--- src/App.php
+++ src/App.php
@@ -4,15 +4,16 @@ namespace App;
 
 use Symfony\Component\Routing\Route;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
 class App
 {
     /**
-     * Enlever dernier "/" dans 'URL'
+     * Enlever le dernier "/" dans 'URL'
      */
     public function run(Request $request): Response
     {
@@ -25,28 +26,21 @@ class App
         }
 
         $routes = new RouteCollection();
-        $routes->add('home', new Route('/'));
-        $routes->add('home_1', new Route('/home'));
-        $routes->add('employes', new Route('/employes'));
-        $routes->add('employes_name', new Route('/employes/{name}'));
+        $routes->add('home', new Route('', ['_controller' => 'home.php']));
+        $routes->add('home_1', new Route('/home', ['_controller' => 'home.php']));
+        $routes->add('employes', new Route('/employes', ['_controller' => 'employes.php']));
+        $routes->add('employes_name', new Route('/employes/{name}', ['_controller' => 'employes.php']));
 
         $context = new RequestContext($request);
         $urlMatcher = new UrlMatcher($routes, $context);
-        $result = $urlMatcher->match($uri);
-        dd($result);
-
-        $map = [
-            '/' => 'home.php',
-            '/home' => 'home.php',
-            '/employes' => 'employes.php'
-        ];
-
-        if (!isset($map[$uri])) {
+        try {
+            $result = $urlMatcher->match($uri);
+        } catch (ResourceNotFoundException $th) {
             return new Response('Page not Found', 404);
         }
 
         ob_start();
-        require __DIR__ . '/../pages/' . $map[$uri];
+        require __DIR__ . '/../pages/' . $result['_controller'];
         $content = ob_get_clean();
 
         $response->setContent($content);
