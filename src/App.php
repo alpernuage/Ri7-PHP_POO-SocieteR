0c6dbc61899eb45ea1f2ab9206c191a8df3f2494
alpernuage <alperakbulut@gmail.com>
2021-10-22 12:28:29 +0200

    add Routes

--- src/App.php
+++ src/App.php
@@ -2,8 +2,12 @@
 
 namespace App;
 
+use Symfony\Component\Routing\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouteCollection;
 
 class App
 {
@@ -20,10 +24,21 @@ class App
             return $response;
         }
 
+        $routes = new RouteCollection();
+        $routes->add('home', new Route('/'));
+        $routes->add('home_1', new Route('/home'));
+        $routes->add('employes', new Route('/employes'));
+        $routes->add('employes_name', new Route('/employes/{name}'));
+
+        $context = new RequestContext($request);
+        $urlMatcher = new UrlMatcher($routes, $context);
+        $result = $urlMatcher->match($uri);
+        dd($result);
+
         $map = [
             '/' => 'home.php',
             '/home' => 'home.php',
-            '/employe' => 'employes.php'
+            '/employes' => 'employes.php'
         ];
 
         if (!isset($map[$uri])) {
